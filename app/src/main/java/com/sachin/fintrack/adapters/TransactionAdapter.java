e00c67657a086300ad55fa811a13f4bd2e46cde9
sachinkumar <allknowledge34@gmail.com>
2025-09-29 23:09:54 +0530

    nine change

--- app/src/main/java/com/sachin/fintrack/adapters/TransactionAdapter.java
+++ app/src/main/java/com/sachin/fintrack/adapters/TransactionAdapter.java
@@ -3,12 +3,12 @@ package com.sachin.fintrack.adapters;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sachin.fintrack.R;
@@ -17,21 +17,25 @@ import com.sachin.fintrack.models.Category;
 import com.sachin.fintrack.models.Transaction;
 import com.sachin.fintrack.utils.Constants;
 import com.sachin.fintrack.utils.Helper;
-import com.sachin.fintrack.views.activites.MainActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
 
-    Context context;
-    List<Transaction> transactions;
+    private final Context context;
+    private final List<Transaction> transactions;
+    private final OnTransactionDeleteListener deleteListener;
 
-    public TransactionAdapter(Context context, List<Transaction> transactions) {
-        this.context = context;
-        this.transactions = new ArrayList<>(transactions);
+    public interface OnTransactionDeleteListener {
+        void onDeleteTransaction(Transaction transaction);
     }
 
+    public TransactionAdapter(Context context, List<Transaction> transactions, OnTransactionDeleteListener deleteListener) {
+        this.context = context;
+        this.transactions = new ArrayList<>(transactions);
+        this.deleteListener = deleteListener;
+    }
 
     @NonNull
     @Override
@@ -44,9 +48,18 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
         Transaction transaction = transactions.get(position);
 
         holder.binding.transactionAmount.setText(String.valueOf(transaction.getAmount()));
-        holder.binding.accountLbl.setText(transaction.getAccount());
-        holder.binding.transactionDate.setText(Helper.formatDate(transaction.getDate()));
-        holder.binding.transactionCategory.setText(transaction.getCategory());
+
+        String account = transaction.getAccount() != null ? transaction.getAccount() : "N/A";
+        holder.binding.accountLbl.setText(account);
+
+        if (transaction.getDate() != null) {
+            holder.binding.transactionDate.setText(Helper.formatDate(transaction.getDate()));
+        } else {
+            holder.binding.transactionDate.setText("");
+        }
+
+        String categoryName = transaction.getCategory() != null ? transaction.getCategory() : "Other";
+        holder.binding.transactionCategory.setText(categoryName);
 
         Category transactionCategory = null;
         if (transaction.getCategory() != null && !transaction.getCategory().isEmpty()) {
@@ -55,42 +68,49 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
 
         if (transactionCategory != null) {
             holder.binding.categoryIcon.setImageResource(transactionCategory.getCategoryImage());
-            holder.binding.categoryIcon.setBackgroundTintList(context.getColorStateList(transactionCategory.getCategoryColor()));
+            holder.binding.categoryIcon.setBackgroundTintList(
+                    ContextCompat.getColorStateList(context, transactionCategory.getCategoryColor())
+            );
         } else {
             holder.binding.categoryIcon.setImageResource(R.drawable.ic_other);
-            holder.binding.categoryIcon.setBackgroundTintList(context.getColorStateList(R.color.redColor));
+            holder.binding.categoryIcon.setBackgroundTintList(
+                    ContextCompat.getColorStateList(context, R.color.redColor)
+            );
         }
 
-        holder.binding.accountLbl.setBackgroundTintList(context.getColorStateList(Constants.getAccountsColor(transaction.getAccount())));
+        holder.binding.accountLbl.setBackgroundTintList(
+                ContextCompat.getColorStateList(context, Constants.getAccountsColor(account))
+        );
 
-        if (transaction.getType() != null) {
-            if (transaction.getType().equals(Constants.INCOME)) {
-                holder.binding.transactionAmount.setTextColor(context.getColor(R.color.greenColor));
-            } else if (transaction.getType().equals(Constants.EXPENSE)) {
-                holder.binding.transactionAmount.setTextColor(context.getColor(R.color.redColor));
-            }
+        if (Constants.INCOME.equals(transaction.getType())) {
+            holder.binding.transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.greenColor));
+        } else if (Constants.EXPENSE.equals(transaction.getType())) {
+            holder.binding.transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.redColor));
+        } else {
+            holder.binding.transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.black));
         }
 
         holder.itemView.setOnLongClickListener(view -> {
-            AlertDialog deleteDialog = new AlertDialog.Builder(context).create();
+            AlertDialog deleteDialog = new AlertDialog.Builder(view.getContext()).create();
             deleteDialog.setTitle("Delete Transaction");
-            deleteDialog.setMessage("Are you sure to delete this transaction?");
+            deleteDialog.setMessage("Are you sure you want to delete this transaction?");
             deleteDialog.setButton(Dialog.BUTTON_POSITIVE, "Yes", (dialogInterface, i) -> {
-                ((MainActivity) context).viewModel.deleteTransaction(transaction);
+                if (deleteListener != null) {
+                    deleteListener.onDeleteTransaction(transaction);
+                }
             });
             deleteDialog.setButton(Dialog.BUTTON_NEGATIVE, "No", (dialogInterface, i) -> deleteDialog.dismiss());
             deleteDialog.show();
-            return false;
+            return true;
         });
     }
-
 
     @Override
     public int getItemCount() {
         return transactions.size();
     }
 
-    public class TransactionViewHolder extends RecyclerView.ViewHolder {
+    public static class TransactionViewHolder extends RecyclerView.ViewHolder {
         RowTransactionBinding binding;
 
         public TransactionViewHolder(@NonNull View itemView) {
--- app/src/main/java/com/sachin/fintrack/views/fragments/TransactionsFragment.java
+++ app/src/main/java/com/sachin/fintrack/views/fragments/TransactionsFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -35,6 +34,11 @@ import java.util.List;
 
 public class TransactionsFragment extends Fragment {
 
+    private FragmentTransactionsBinding binding;
+    private FirebaseFirestore firestore;
+    private Calendar calendar;
+    public MainViewModel viewModel;
+
     public TransactionsFragment() {
         // Required empty public constructor
     }
@@ -44,87 +48,77 @@ public class TransactionsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    FragmentTransactionsBinding binding;
-    FirebaseFirestore firestore;
-
-    Calendar calendar;
-
-    /*
-    0 = Daily
-    1 = Monthly
-    2 = Calender
-    3 = Summary
-    4 = Notes
-     */
-
-    public MainViewModel viewModel;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentTransactionsBinding.inflate(inflater);
+        binding = FragmentTransactionsBinding.inflate(inflater, container, false);
 
         viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
-
         firestore = FirebaseFirestore.getInstance();
 
         loadUserData();
 
         calendar = Calendar.getInstance();
         updateDate();
-        binding.nextDateBtn.setOnClickListener(c-> {
-            if (Constants.SELECTED_TAB == Constants.DAILY){
+
+        binding.nextDateBtn.setOnClickListener(c -> {
+            if (Constants.SELECTED_TAB == Constants.DAILY) {
                 calendar.add(Calendar.DATE, 1);
-            } else if (Constants.SELECTED_TAB ==Constants.MONTHLY) {
+            } else if (Constants.SELECTED_TAB == Constants.MONTHLY) {
                 calendar.add(Calendar.MONTH, 1);
             }
             updateDate();
         });
 
-        binding.previousDateBtn.setOnClickListener(c-> {
-            if (Constants.SELECTED_TAB == Constants.DAILY){
+        binding.previousDateBtn.setOnClickListener(c -> {
+            if (Constants.SELECTED_TAB == Constants.DAILY) {
                 calendar.add(Calendar.DATE, -1);
-            } else if (Constants.SELECTED_TAB ==Constants.MONTHLY) {
+            } else if (Constants.SELECTED_TAB == Constants.MONTHLY) {
                 calendar.add(Calendar.MONTH, -1);
             }
             updateDate();
         });
 
-        binding.floatingActionButton.setOnClickListener(c-> {
+        binding.floatingActionButton.setOnClickListener(c -> {
             new AddTransactionFragment().show(getParentFragmentManager(), null);
         });
 
         binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getText().equals("Monthly")){
-                    Constants.SELECTED_TAB = 1;
+                if ("Monthly".contentEquals(tab.getText())) {
+                    Constants.SELECTED_TAB = Constants.MONTHLY;
                     updateDate();
-                } else if (tab.getText().equals("Daily")) {
-                    Constants.SELECTED_TAB = 0;
+                } else if ("Daily".contentEquals(tab.getText())) {
+                    Constants.SELECTED_TAB = Constants.DAILY;
                     updateDate();
                 }
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                // Not needed
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
+                // Not needed
             }
         });
 
-        binding.transactionsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.transactionsList.setLayoutManager(new LinearLayoutManager(requireContext()));
 
         viewModel.transactions.observe(getViewLifecycleOwner(), new Observer<List<Transaction>>() {
             @Override
             public void onChanged(List<Transaction> transactions) {
-                TransactionAdapter transactionAdapter = new TransactionAdapter(getActivity(), transactions);
+                TransactionAdapter transactionAdapter = new TransactionAdapter(
+                        requireContext(),
+                        transactions,
+                        transaction -> viewModel.deleteTransaction(transaction) // delete action
+                );
                 binding.transactionsList.setAdapter(transactionAdapter);
-                if(transactions.size() > 0) {
+
+                if (transactions != null && transactions.size() > 0) {
                     binding.emptyState.setVisibility(View.GONE);
                 } else {
                     binding.emptyState.setVisibility(View.VISIBLE);
@@ -132,28 +126,20 @@ public class TransactionsFragment extends Fragment {
             }
         });
 
-        viewModel.totalIncome.observe(getViewLifecycleOwner(), new Observer<Double>() {
-            @Override
-            public void onChanged(Double aDouble) {
-                binding.incomeLbl.setText(String.valueOf(aDouble));
-            }
+        viewModel.totalIncome.observe(getViewLifecycleOwner(), aDouble -> {
+            binding.incomeLbl.setText(String.valueOf(aDouble));
         });
 
-        viewModel.totalExpense.observe(getViewLifecycleOwner(), new Observer<Double>() {
-            @Override
-            public void onChanged(Double aDouble) {
-                binding.expenseLbl.setText(String.valueOf(aDouble));
-            }
+        viewModel.totalExpense.observe(getViewLifecycleOwner(), aDouble -> {
+            binding.expenseLbl.setText(String.valueOf(aDouble));
         });
 
-        viewModel.totalAmount.observe(getViewLifecycleOwner(), new Observer<Double>() {
-            @Override
-            public void onChanged(Double aDouble) {
-                binding.totalLbl.setText(String.valueOf(aDouble));
-            }
+        viewModel.totalAmount.observe(getViewLifecycleOwner(), aDouble -> {
+            binding.totalLbl.setText(String.valueOf(aDouble));
         });
 
         viewModel.getTransactions(calendar);
+
         return binding.getRoot();
     }
 
@@ -167,33 +153,38 @@ public class TransactionsFragment extends Fragment {
         );
     }
 
-    void updateDate() {
-        if(Constants.SELECTED_TAB == Constants.DAILY) {
+    private void updateDate() {
+        if (Constants.SELECTED_TAB == Constants.DAILY) {
             binding.currentDate.setText(Helper.formatDate(calendar.getTime()));
-        } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+        } else if (Constants.SELECTED_TAB == Constants.MONTHLY) {
             binding.currentDate.setText(Helper.formatDateByMonth(calendar.getTime()));
         }
         viewModel.getTransactions(calendar);
     }
 
     private void loadUserData() {
+        firestore.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        UserModel model = documentSnapshot.toObject(UserModel.class);
+                        if (documentSnapshot.exists() && model != null) {
+                            binding.userName.setText(model.getName() != null ? model.getName() : "Guest");
 
-        firestore.collection("users").document(FirebaseAuth.getInstance().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            Picasso.get()
+                                    .load(model.getProfile())
+                                    .placeholder(R.drawable.friend_2)
+                                    .into(binding.profileImage);
+                        }
+                    }
+                });
+    }
 
-                UserModel model = documentSnapshot.toObject(UserModel.class);
-
-                if (documentSnapshot.exists()){
-                    binding.userName.setText(model.getName());
-
-                    Picasso.get()
-                            .load(model.getProfile())
-                            .placeholder(R.drawable.friend_2)
-                            .into(binding.profileImage);
-
-                }
-            }
-        });
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
